867c0b803b0396ce55144809e0735cc85ddb5809
williamyoel <158503721+williamyoel@users.noreply.github.com>
2024-12-18 22:36:21 -0500

    web.php

--- routes/web.php
+++ routes/web.php
@@ -8,42 +8,39 @@ use App\Http\Controllers\HelpController;
 use App\Http\Controllers\SuggestionController;
 use App\Http\Controllers\DataController;
 
-
-
-// Rutas protegidas por autenticación
-Route::middleware('auth')->group(function () {
-    // Mostrar perfil del usuario
-    Route::get('/usuario', [UserController::class, 'index'])->name('usuario.index');
-
-    // Editar perfil del usuario
-    Route::get('/usuario/editar', [UserController::class, 'edit'])->name('usuario.editar');
-    Route::post('/usuario/editar', [UserController::class, 'update'])->name('usuario.update');
-});
-
-
-
-
 // Página principal
 Route::get('/', [AuthController::class, 'showLogin'])->name('home');
 
 // Autenticación
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+Route::prefix('auth')->group(function () {
+    Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
-// Usuario
-Route::get('/usuario', [UserController::class, 'index'])->name('usuario.index');
+// Rutas protegidas por autenticación
+Route::middleware(['auth'])->group(function () {
+    // Usuario
+    Route::prefix('usuario')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('usuario.index');
+        Route::get('/perfil', function () {
+            return view('usuario.index');
+        })->name('usuario.perfil');
+        Route::get('/editar', [UserController::class, 'edit'])->name('usuario.editar');
+        Route::post('/editar', [UserController::class, 'update'])->name('usuario.update');
+    });
 
-// Biblioteca
-Route::get('/libros', [LibraryController::class, 'index'])->name('biblioteca.index');
+    // Biblioteca
+    Route::get('/libros', [LibraryController::class, 'index'])->name('biblioteca.index');
 
-// Soporte y ayuda
-Route::get('/soporte', [HelpController::class, 'index'])->name('soporte.index');
+    // Soporte y ayuda
+    Route::get('/soporte', [HelpController::class, 'index'])->name('soporte.index');
 
-// Sugerencias y aportes
-Route::get('/sugerencias', [SuggestionController::class, 'index'])->name('sugerencias.index');
+    // Sugerencias y aportes
+    Route::get('/sugerencias', [SuggestionController::class, 'index'])->name('sugerencias.index');
 
-// Datos abiertos
-Route::get('/datos', [DataController::class, 'index'])->name('datos.index');
+    // Datos abiertos
+    Route::get('/datos', [DataController::class, 'index'])->name('datos.index');
+});
